444ef195201b1d808ba3c8482b9801d34f97f20f
SimardeepSingh1450 <smudhar_be21@thapar.edu>
2022-08-31 12:34:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
-
+import ReactMarkdown from 'react-markdown'
+import acm from './assets/acmthapar.jpg'
+import { motion } from "framer-motion"
 function App() {
+  const [lefttext,setLeftText]=useState('Mark Down Editor');
+  const [loading,setLoading]=useState(false);
+
+  useEffect(()=>{
+      setLoading(true);
+      setTimeout(()=>{
+        setLoading(false);
+      },5000)
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        loading?<>
+        <motion.div animate={{scale:[1,1.4,1]}} initial={{scale:1}} transition={{type:'spring',duration:2,repeat:Infinity}} className='loaderdiv'><img className='mimage2' alt="mimage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/1200px-Markdown-mark.svg.png"/><img className='acmimg' alt="acm" src={acm}/></motion.div></>:
+      <>
+      <div style={{display:'flex',justifyContent:'center',marginTop:5}}><img className='mimage' alt="mimage" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Markdown-mark.svg/1200px-Markdown-mark.svg.png"/><h1>&nbsp;&nbsp;MarkDown Editor</h1></div>
+      <div className='flexer'>
+        <textarea value={lefttext} className='leftside' onChange={(e)=>{setLeftText(e.target.value)}}/>
+        <div className='rightside'><ReactMarkdown>{lefttext}</ReactMarkdown></div>
+      </div>
+      </>
+      }
     </div>
   );
 }
